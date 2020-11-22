d800bfd6144d82697e4be8f073805fc60e8c285f
Michael Romano <michaelfromano@gmail.com>
2020-11-22 15:49:23 -0500

    updated movie_maker.py

--- movie_maker.py
+++ movie_maker.py
@@ -7,84 +7,149 @@ Created on Sat Apr 11 15:04:13 2020
 """
 
 import cv2
-from tkinter import filedialog, Tk
+import tkinter.filedialog
+import tkinter as tk
 import numpy as np
+import matplotlib.pyplot as plt
+import os
 
 
-def prompt_fnames():
-    filenames = filedialog.askopenfilenames(initialdir='.', title='Select files', filetypes=(('jpg files', '*.jpg'),
-                                                                                             ('all files', '*.*'),
-                                                                                             ('png files', '*.png')))
-    return filenames
+class MakeCine(object):
 
+    def __init__(self):
+        self._prompt_fnames()
+        self._prompt_output_name()
 
-def auto_crop(filenames, ref_no=0):
-    first_file = cv2.imread(filenames[ref_no])
-    non_red = identify_red_regions(first_file)
+    def _prompt_fnames(self):
+        root = tk.Tk()
+        filenames = tkinter.filedialog.askopenfilenames(parent=root,
+                initialdir='.', title='Select files',
+                filetypes=(
+                        ('tiff files', '*.tif'),
+                ))
+        self.filenames = filenames
+        root.destroy()
 
-    first_file = cv2.cvtColor(first_file, cv2.COLOR_BGR2GRAY)
-    first_file = cv2.bitwise_and(first_file, non_red)
-    # first apply morphological
-    kernel = np.ones((5, 5), dtype=np.uint8)
-    _, mask = cv2.threshold(first_file, thresh=0, maxval=1, type=cv2.THRESH_BINARY)
-    mask[np.where(mask > 0)] = 1
-    dilate = cv2.morphologyEx(mask, cv2.MORPH_DILATE, kernel)
-    return dilate
+    def _prompt_output_name(self):
+        root = tk.Tk()
+        tk.Label(root, text='Please enter output filename: ').grid(row=0)
+        # tk.Label(root, text='Please enter FPS: ').grid(row=1)
+        fname = tk.Entry(root)
+        fname.grid(row=0, column=1)
+        self.outputname = ''
+        def enter(event):
+            self.outputname = fname.get()
+            root.destroy()
+        root.bind('<Return>', enter)
+        tk.mainloop()
 
+    def load_videos(self):
+        img = cv2.imread(self.filenames[0])
+        img_stack = np.expand_dims(img, axis=3)
+        for filename in self.filenames[1:]:
+            img = cv2.imread(filename)
+            img_stack = np.append(img_stack, np.expand_dims(img, axis=3), axis=3)
+        self.img_stack = img_stack
 
-def bounding_box(img):
-    x, y, w, h = cv2.boundingRect(img)
-    return [x,y,w,h]
+    def remove_white_regions(self):
+        img = np.squeeze(self.img_stack[:,:,:,0])
+        white_mask = np.sum(img > 250, axis=2) == 3
+        # upper mask (170-180)
+        kernel = np.ones((10, 10), dtype=np.uint8)
+        opening = cv2.dilate(white_mask.astype(np.uint8), kernel)
+        [x,y,w,h] = self.bounding_box(opening)
+        white_mask = 1-white_mask
+        white_mask[y:y + h, x:x + w] = 0
+        for i in range(self.img_stack.shape[-1]):
+            for j in range(self.img_stack.shape[-2]):
+                    self.img_stack[:,:,j,i] = cv2.bitwise_and(
+                    np.squeeze(self.img_stack[:,:,j,i]),
+                    np.squeeze(self.img_stack[:,:,j,i]),
+                    mask=white_mask.astype(np.uint8))
+        np_mask = np.squeeze(np.sum(self.img_stack[:,:,:,0] > 0, axis=2))
+        [x, y, w, h] = self.bounding_box(np_mask.astype(np.uint8))
+        self.img_stack = self.img_stack[y:y + h, x:x + w,:,:]
 
+    def bounding_box(self, img):
+        x, y, w, h = cv2.boundingRect(img)
+        return [x,y,w,h]
 
-def apply_bounding_box(image_files, bb):
-    img = cv2.imread(image_files[0])
-    [x,y,w,h] = bb
-    img_stack = np.expand_dims(img[y:y + h, x:x + w, :], axis=3)
-    for filename in image_files[1:]:
-        img = cv2.imread(filename)
-        img_stack = np.append(img_stack,np.expand_dims(img[y:y+h, x:x+w, :], axis=3), axis=3)
-    return img_stack
+    def interpolate_frames(self, multiplier=10):
+        sz = self.img_stack.shape
+        new_img_stack = np.empty((sz[0:3] + ((sz[3]-1)*multiplier,)))
+        for i in range(sz[-1]-1):
+            for j in range(multiplier):
+                a = 1-(j/multiplier)
+                b = j/multiplier
+                new_img_stack[:,:,:,i*multiplier+j] = \
+                np.round(self.img_stack[:,:,:,i]*a + \
+                self.img_stack[:,:,:,i+1]*b)
+        self.img_stack = new_img_stack.astype(np.uint8)
 
-# from https://stackoverflow.com/questions/30331944/finding-red-color-in-image-using-python-opencv, user derricw
-def identify_red_regions(img):
-    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    def compute_and_write_gradient(self):
+        self.gradient = np.gradient(self.img_stack, axis=3)
 
-    # lower mask (0-1, 0)
-    lower_red = np.array([0, 50, 50])
-    upper_red = np.array([10, 255, 255])
-    mask0 = cv2.inRange(img_hsv, lower_red, upper_red)
+    def write_video(self, fps=30):
+        codec = cv2.VideoWriter_fourcc(*'XVID')  # use the mp4v codec
+        shape = self.img_stack.shape[0:2]
+        output_writer = cv2.VideoWriter(os.path.join(os.path.abspath('.'),
+                                                      self.outputname),
+                                        codec,
+                        float(fps), (shape[1],shape[0]))
+        for im_index in range(self.img_stack.shape[-1]):
+            output_writer.write(np.squeeze(self.img_stack[:, :, :, im_index]))
+        output_writer.release()
 
-    # upper mask (170-180)
-    lower_red = np.array([170, 50, 50])
-    upper_red = np.array([180, 255, 255])
-    mask1 = cv2.inRange(img_hsv, lower_red, upper_red)
-
-    # join my masks
-    mask = mask0 + mask1
-
-    # set my output img to zero everywhere except my mask
-    output_img = img.copy()
-    output_img[np.where(mask == 0)] = 1
-    output_img[np.where(mask != 0)] = 0
-    kernel = np.ones((20, 20), dtype=np.uint8)
-    opening = cv2.morphologyEx(output_img, cv2.MORPH_ERODE, kernel)
-    output_img = cv2.cvtColor(opening, cv2.COLOR_BGR2GRAY)
-
-    return output_img
-
-def write_video(output_filename, image_stack, fps=5):
-    codec = cv2.VideoWriter_fourcc(*'MP42')  # use the mp4v codec
-    shape = image_stack.shape[0:2]
-    output_writer = cv2.VideoWriter(output_filename, codec, float(fps), (shape[1], shape[0]))
-    for im_index in range(image_stack.shape[-1]):
-        output_writer.write(np.squeeze(image_stack[:, :, :, im_index]))
-    output_writer.release()
-
+    def write_gradient(self, fps=30):
+        codec = cv2.VideoWriter_fourcc(*'XVID')  # use the mp4v codec
+        shape = self.gradient.shape[0:2]
+        output_writer = cv2.VideoWriter(os.path.join(os.path.abspath('.'),
+                                                     'gradient_' +
+        self.outputname),
+                                        codec,
+                                        float(fps), (shape[1], shape[0]))
+        for im_index in range(self.gradient.shape[-1]):
+            output_writer.write(
+                np.squeeze(self.gradient[:, :, :, im_index]))
+        output_writer.release()
 
 if __name__ == '__main__':
-    # filenames = prompt_fnames()
-    dilate = auto_crop(filenames)
-    bb = bounding_box(dilate)
-    img_stack = apply_bounding_box(filenames, bb)
-    write_video('hello_world.avi', img_stack)
+    cine = MakeCine()
+    cine.load_videos()
+    cine.remove_white_regions()
+    cine.interpolate_frames()
+    cine.compute_and_write_gradient()
+    cine.write_gradient()
+    cine.write_video()
+
+    #
+    # # from https://stackoverflow.com/questions/30331944/finding-red-color-in-image-using-python-opencv, user derricw
+    # def identify_red_regions(self, img):
+    #     img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    #
+    #     # lower mask (0-1, 0)
+    #     lower_red = np.array([0, 30, 30])
+    #     upper_red = np.array([10, 255, 255])
+    #     mask0 = cv2.inRange(img_hsv, lower_red, upper_red)
+    #
+    #     # upper mask (170-180)
+    #     lower_red = np.array([170, 30, 30])
+    #     upper_red = np.array([180, 255, 255])
+    #     mask1 = cv2.inRange(img_hsv, lower_red, upper_red)
+    #
+    #     # join my masks
+    #     mask = mask0 + mask1
+    #
+    #     # set my output img to zero everywhere except my mask
+    #     output_img = img.copy()
+    #     output_img[np.where(mask == 0)] = 0
+    #     output_img[np.where(mask != 0)] = 1
+    #     output_img = np.sum(output_img,axis=2)
+    #     output_img[output_img > 1] = 1
+    #     output_img = output_img.astype('uint8')
+    #     kernel = np.ones((30, 30), dtype=np.uint8)
+    #     opening = cv2.dilate(output_img, kernel)
+    #     [x,y,w,h] = self.bounding_box(opening)
+    #     opening[y:y + h, x:x + w] = 1
+    #     opening = cv2.bitwise_not(opening)
+    #     return opening
