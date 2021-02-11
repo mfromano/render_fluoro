45d2f733792294aab21bf883ba18a4fc8af1a95d
mfromano <michaelfromano@gmail.com>
2021-02-11 15:24:42 -0500

    updated main.py

--- main.py
+++ main.py
@@ -11,11 +11,20 @@ for dir in dirs:
     prefix = os.path.split(init_dir)[1]
     curdir = os.path.join(init_dir, dir)
     len_dir = len(os.listdir(curdir))
+    # if len_dir > 10:
+    #     fps_in = 5
+    #     fps_out = 10
+    #     scaling = 2
+    #     batch_size = 5
     if len_dir > 1:
-        fps_out = 5
-        scaling = 1
-        newname = '_'.join([prefix, dir])
-        print(f'python movie_maker.py --indir {os.path.join(init_dir,dir)} ' +\
-                        f'--outputpref {newname} --fps_in 5 --fps_out {fps_out} --scaling_factor {scaling}')
-        subprocess.run( f'python movie_maker.py --indir {os.path.join(init_dir,dir)} ' +\
-                        f'--outputpref {newname} --fps_in 5 --fps_out {fps_out} --scaling_factor {scaling}', shell=True)
+        fps_in = 2
+        fps_out = 10
+        scaling = 5
+        batch_size = 2
+    else:
+        continue
+    newname = '_'.join([prefix, dir])
+    print(f'python movie_maker.py --indir {os.path.join(init_dir,dir)} ' +\
+                    f'--outputpref {newname} --fps_in 5 --fps_out {fps_out} --scaling_factor {scaling} --batch_size {batch_size}')
+    subprocess.run( f'python movie_maker.py --indir {os.path.join(init_dir,dir)} ' +\
+                    f'--outputpref {newname} --fps_in 5 --fps_out {fps_out} --scaling_factor {scaling} --batch_size {batch_size}', shell=True)
